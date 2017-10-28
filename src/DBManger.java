eda530df128fb8f086928704637140eab4f10e9a
nbc <nbc@EIM-USA-003>
2017-10-27 17:24:26 -0700

    Commitment made on 10/27/2017

--- src/DBManger.java
+++ src/DBManger.java
@@ -1,8 +1,12 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import javax.sql.DataSource;
+
+
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class DBManger {
@@ -12,35 +16,31 @@ public class DBManger {
 	private static final String Username = "root";
 	private static final String Password = "200020";
 
+	// Generate a DataSource of a database by read configuration file.
 	public static DataSource getMySQLDataSource() {
 		
 		MysqlDataSource mysqlDS = new MysqlDataSource();
-		
 		mysqlDS.setURL("jdbc:mysql://localhost:3306/NQWarehouse");
 		mysqlDS.setUser(Username);
 		mysqlDS.setPassword(Password);
-		
 		return mysqlDS;
 		
 	}
 	
-	private static void LoadDataBase() {
-		 ds = DBManger.getMySQLDataSource();
-	}
-	
-	public static void TestDataBase() {
+	// Create a server table
+	public static void CreateServerTable() {
 		
 		try {
 			Connection con = ds.getConnection();
 			Statement stmt = con.createStatement();
+			
 			String Query = "CREATE TABLE ServerInventory (\n 	ProductNumber VARCHAR(255) DEFAULT NULL,\n"
 					+ "	ProductName VARCHAR(255) DEFAULT NULL,\n"
 					+ " 	SerialNumber VARCHAR(255) NOT NULL UNIQUE,\n"
 					+ " 	FRUType VARCHAR(255) DEFAULT NULL,\n"
 					+ " 	PRIMARY KEY (SerialNumber));";
-			System.out.println(Query + "\n");
 			
-			// Create a server table
+			System.out.println(Query + "\n");
 			stmt.executeUpdate(Query);
 			con.close();
 		}
@@ -48,7 +48,11 @@ public class DBManger {
 		catch(SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	// Use Case: determine a specific part by given Part Number
+	public static void PartNumberSearching(String pn) {
+
 		
 	}
-
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 import javax.sql.DataSource;
 
 /* Use Case
@@ -9,15 +10,22 @@ import javax.sql.DataSource;
  
  */
 
-
 public class Main {
-
+	private static DataSource ds = null;
+	
 	public static void main(String[] args) {
-		LoadDataBase();
-		TestDataBase();
+		ds = DBManger.getMySQLDataSource(); // Get the database instance
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Welcome to NQ-Kaminario Console. How can I assist you?\n"
+				+ "1. Received an item");
+		
+		while(sc.hasNext()){
+			if (sc.next().equals("1")) {
+				System.out.println("Please enter the part number.");
+			}
+			
+		}
 	}
-	
-
-	
 }
 
